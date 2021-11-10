3b41b6b1ad244818bf93274335b185d18d54ff9f
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2021-11-10 16:30:19 +0100

    [TEST] ParamValue compiles.

--- test/ParamValue_test.cpp
+++ test/ParamValue_test.cpp
@@ -1,36 +1,38 @@
-// --------------------------------------------------------------------------
-//                   OpenMS -- Open-Source Mass Spectrometry
-// --------------------------------------------------------------------------
-// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
-// ETH Zurich, and Freie Universitaet Berlin 2002-2021.
-//
-// This software is released under a three-clause BSD license:
-//  * Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//  * Neither the name of any author or any participating institution
-//    may be used to endorse or promote products derived from this software
-//    without specific prior written permission.
-// For a full list of authors, refer to the file AUTHORS.
-// --------------------------------------------------------------------------
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
-// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// --------------------------------------------------------------------------
-// $Authors: Ruben Grünberg $
-// --------------------------------------------------------------------------
+#include <gtest/gtest.h>
 
+#include <sstream>
+#include <iostream>
+
+#include <tdl/ParamValue.h>
+
+TEST(ParamValue_construction, default_construction)
+{
+        tdl::ParamValue* dv_ptr = nullptr;
+        tdl::ParamValue* dv_nullPointer = nullptr;
+
+        // Just as a sanity check, the size of ParamValue should be exactly 16 bytes
+        // on a 64 bit system:
+        // - 1 byte for the data type
+        // - 7 byte padding
+        // - 8 bytes for the actual data / pointers to data
+        EXPECT_EQ(sizeof(tdl::ParamValue), 16);
+
+        dv_ptr = new tdl::ParamValue;
+        EXPECT_NE(dv_ptr, dv_nullPointer);
+}
+
+TEST(ParamValue_construction, destructor)
+{
+        tdl::ParamValue* dv_ptr = nullptr;
+        delete dv_ptr;
+}
+
+// TODO adapt all tests below to google test (only default construction and deconstruction was done)
+// each START_SECTION / END_SECTION should be a seprate TEST()
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+/*
 #include <OpenMS/CONCEPT/ClassTest.h>
 
 ///////////////////////////
@@ -828,6 +830,4 @@ START_TEST(ParamValue, "$Id$")
 /////////////////////////////////////////////////////////////
 END_TEST
 
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
+ */
