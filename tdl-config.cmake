297b1ce15a5eb38bd26821a8c3589b1ced29204d
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-07-26 12:51:11 +0200

    infra: add install script for cmake

--- tdl-config.cmake
+++ tdl-config.cmake
@@ -7,32 +7,44 @@
 
 cmake_minimum_required (VERSION 3.12)
 if (TARGET tdl)
-    return()
-endif()
+    return ()
+endif ()
+
+option (INSTALL_TDL "Enable installation of TDL. (Projects embedding TDL may want to turn this OFF.)" ON)
 
 find_package (yaml-cpp QUIET)
-
 if (NOT yaml-cpp_FOUND)
-    message (STATUS "Fetching yaml-cpp")
+    if (NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+        message (STATUS "Fetching yaml-cpp")
+    endif ()
 
     include (FetchContent)
     FetchContent_Declare (
         yaml-cpp_fetch_content
         GIT_REPOSITORY "https://github.com/jbeder/yaml-cpp.git"
-        GIT_TAG "35b4498026b6293bfadc75f9ee325cb16d6975af") # 0.7.0 can't be properly installed
+        # !WORKAROUND git tag 0.7.0 can't be properly installed
+        GIT_TAG "35b4498026b6293bfadc75f9ee325cb16d6975af")
     option (YAML_CPP_BUILD_CONTRIB "" OFF)
     option (YAML_CPP_BUILD_TOOLS "" OFF)
     option (YAML_BUILD_SHARED_LIBS "" OFF)
-    option (YAML_CPP_INSTALL "" ON)
+    option (YAML_CPP_INSTALL "" ${INSTALL_TDL})
+    option (YAML_CPP_BUILD_TESTS "" OFF)
     set_property (GLOBAL PROPERTY CTEST_TARGETS_ADDED 1)
     FetchContent_MakeAvailable (yaml-cpp_fetch_content)
 else ()
-    message (STATUS "Found yaml-cpp ${yaml-cpp_VERSION}")
+    if (NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+        message (STATUS "Found yaml-cpp")
+    endif ()
 endif ()
 
-add_library(tdl INTERFACE)
-target_include_directories(tdl INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/src>
-    $<INSTALL_INTERFACE:include>)
-target_link_libraries(tdl INTERFACE yaml-cpp)
+add_library (tdl INTERFACE)
+target_include_directories (tdl INTERFACE
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/src>"
+    "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
+# !WORKAROUND yaml-cpp::yaml-cpp does not work for 0.7.0 if installed on system
+target_link_libraries (tdl INTERFACE yaml-cpp)
 add_library (tdl::tdl ALIAS tdl)
+
+if (INSTALL_TDL)
+    include (${CMAKE_CURRENT_LIST_DIR}/cmake/install.cmake)
+endif ()
