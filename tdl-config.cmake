c65ab3c1d0283dc0605ad19890f1d3e1a8f4adda
Enrico Seiler <enrico.seiler@hotmail.de>
2023-10-09 09:41:35 +0200

    [INFRA] Update yaml-cpp

--- tdl-config.cmake
+++ tdl-config.cmake
@@ -9,7 +9,7 @@ endif ()
 
 option (INSTALL_TDL "Enable installation of TDL. (Projects embedding TDL may want to turn this OFF.)" ON)
 
-find_package (yaml-cpp QUIET)
+find_package (yaml-cpp 0.8.0 QUIET)
 if (NOT yaml-cpp_FOUND)
     if (NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
         message (STATUS "Fetching yaml-cpp")
@@ -19,8 +19,8 @@ if (NOT yaml-cpp_FOUND)
     FetchContent_Declare (
         yaml-cpp_fetch_content
         GIT_REPOSITORY "https://github.com/jbeder/yaml-cpp.git"
-        # !WORKAROUND git tag 0.7.0 can't be properly installed due to missing CMake
-        GIT_TAG "35b4498026b6293bfadc75f9ee325cb16d6975af")
+        # !WORKAROUND Points to first commit after 0.8.0: Fixes CMake deprecation warnings (cmake_minimum_required)
+        GIT_TAG "c2680200486572baf8221ba052ef50b58ecd816e")
     option (YAML_CPP_BUILD_CONTRIB "" OFF)
     option (YAML_CPP_BUILD_TOOLS "" OFF)
     option (YAML_BUILD_SHARED_LIBS "" OFF)
@@ -37,13 +37,7 @@ endif ()
 add_library (tdl INTERFACE)
 target_include_directories (tdl INTERFACE "$<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/src>"
                                           "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
-# !WORKAROUND yaml-cpp::yaml-cpp was only defined after the 0.7.0 release
-# yaml-cpp::yaml-cpp is required to correctly link against a fetch_content'ed yaml-cpp
-if (TARGET yaml-cpp::yaml-cpp)
-    target_link_libraries (tdl INTERFACE yaml-cpp::yaml-cpp)
-else ()
-    target_link_libraries (tdl INTERFACE yaml-cpp)
-endif ()
+target_link_libraries (tdl INTERFACE yaml-cpp::yaml-cpp)
 add_library (tdl::tdl ALIAS tdl)
 
 if (INSTALL_TDL)
